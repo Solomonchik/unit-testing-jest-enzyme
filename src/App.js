7c33e1ec40a9d33950ac05deb9e1bddb554a86c8
Solomonchik <viktorzimmermann11@web.de>
2019-09-04 10:18:21 +0200

    Headline components with Tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/header/index";
+import Headline from "./components/headline/index";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="main" data-test="main">
+        <Headline header={'Posts'} description={'Click the button to render props'}/>
+      </section>
     </div>
   );
 }
